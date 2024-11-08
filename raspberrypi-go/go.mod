fdfe3f57d0e4e9cab099ebea81b589b564bde749
muhfurqans35 <muhfurqans35@gmail.com>
2024-11-09 06:14:54 +0800

    Implementasi raspberry pi

--- raspberrypi-go/go.mod
+++ raspberrypi-go/go.mod
@@ -1,3 +1,8 @@
 module fotocopyki-raspberrypi
 
 go 1.19
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/muhfurqans35/print-raspberry-pi v0.0.0-20241107204050-a53b6399d7a5 // indirect
+)
--- raspberrypi-go/handlers/print_handler.go
+++ raspberrypi-go/handlers/print_handler.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 	"strconv"
 
-	"github.com/your_project/models"
+	"github.com/muhfurqans35/print-raspberry-pi/tree/v1/raspberrypi-go/models"
 )
 
 // Request struct untuk menerima data dari frontend
